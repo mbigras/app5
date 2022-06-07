ca39cf031a62356398109f43ab4d95c56409f6e7
Max Bigras <mbigras22@gmail.com>
2022-06-06 21:16:25 -0700

    BREAKING CHANGE: Print JSON

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-__version__ = "1.0.0"
+import json
+
+app = "app"
+
+__version__ = "2.0.0"
+
+features = [
+    "feature1",
+]
 
 
 def main():
-    print(f"app version {__version__}")
+    output = {
+        "app": app,
+        "version": __version__,
+        "features": features,
+    }
+    print(json.dumps(output, indent=2, sort_keys=True))
 
 
 if __name__ == "__main__":
